14a43e14c66bc493dcc68d8bbdda94074c987d72
Nils Elveros <nils.elveros@tromb.com>
2026-02-20 13:44:44 +0100

    fix: update framework-regel version

--- src/main/java/se/fk/rimfrost/framework/regel/maskinell/logic/RegelMaskinellMapper.java
+++ src/main/java/se/fk/rimfrost/framework/regel/maskinell/logic/RegelMaskinellMapper.java
@@ -2,12 +2,18 @@ package se.fk.rimfrost.framework.regel.maskinell.logic;
 
 import java.time.OffsetDateTime;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import se.fk.rimfrost.framework.kundbehovsflode.adapter.dto.KundbehovsflodeResponse;
 import se.fk.rimfrost.framework.regel.logic.dto.FSSAinformation;
 import se.fk.rimfrost.framework.regel.logic.dto.UppgiftStatus;
 import se.fk.rimfrost.framework.regel.logic.entity.ImmutableRegelResult;
+import se.fk.rimfrost.framework.regel.logic.entity.ImmutableUppgiftData;
 import se.fk.rimfrost.framework.regel.logic.entity.RegelResult;
+import se.fk.rimfrost.framework.regel.maskinell.logic.dto.ImmutableErsattning;
+import se.fk.rimfrost.framework.regel.maskinell.logic.dto.ImmutableRegelMaskinellRequest;
+import se.fk.rimfrost.framework.regel.maskinell.logic.dto.RegelMaskinellRequest;
 import se.fk.rimfrost.framework.regel.maskinell.logic.dto.RegelMaskinellResult;
 
 @ApplicationScoped
@@ -17,16 +23,43 @@ public class RegelMaskinellMapper
    public RegelResult toRegelResult(RegelMaskinellResult result)
    {
 
-      return ImmutableRegelResult.builder()
+      var uppgiftData = ImmutableUppgiftData.builder()
             .uppgiftId(UUID.randomUUID())
             .skapadTs(OffsetDateTime.now())
             .planeradTs(OffsetDateTime.now())
             .utfordTs(OffsetDateTime.now())
             .uppgiftStatus(UppgiftStatus.AVSLUTAD)
             .fssaInformation(FSSAinformation.HANDLAGGNING_PAGAR)
+            .build();
+
+      return ImmutableRegelResult.builder()
+            .uppgiftData(uppgiftData)
             .addAllErsattningar(result.ersattningar())
             .addAllUnderlag(result.underlag())
             .utfall(result.utfall())
+            .build();
+   }
+
+   public RegelMaskinellRequest toRegelMaskinellRequest(KundbehovsflodeResponse kundbehovsflodeResponse)
+   {
+      return ImmutableRegelMaskinellRequest.builder()
+            .kundbehovsflodeId(kundbehovsflodeResponse.kundbehovsflodeId())
+            .personnummer(kundbehovsflodeResponse.personnummer())
+            .formanstyp(kundbehovsflodeResponse.formanstyp())
+            .ersattning(
+                  kundbehovsflodeResponse.ersattning()
+                        .stream()
+                        .map(e -> ImmutableErsattning.builder()
+                              .ersattningsId(e.ersattningsId())
+                              .ersattningsTyp(e.ersattningsTyp())
+                              .omfattningsProcent(e.omfattningsProcent())
+                              .belopp(e.belopp())
+                              .berakningsgrund(e.berakningsgrund())
+                              .beslutsutfall(e.beslutsutfall())
+                              .franOchMed(e.franOchMed())
+                              .tillOchMed(e.tillOchMed())
+                              .build())
+                        .collect(Collectors.toList()))
             .build();
    }
 }
--- src/main/java/se/fk/rimfrost/framework/regel/maskinell/logic/RegelMaskinellServiceInterface.java
+++ src/main/java/se/fk/rimfrost/framework/regel/maskinell/logic/RegelMaskinellServiceInterface.java
@@ -1,10 +1,9 @@
 package se.fk.rimfrost.framework.regel.maskinell.logic;
 
-import se.fk.rimfrost.framework.regel.logic.dto.ProcessRegelRequest;
+import se.fk.rimfrost.framework.regel.maskinell.logic.dto.RegelMaskinellRequest;
 import se.fk.rimfrost.framework.regel.maskinell.logic.dto.RegelMaskinellResult;
 
 public interface RegelMaskinellServiceInterface
 {
-
-   RegelMaskinellResult processRegel(ProcessRegelRequest regelResult);
+   RegelMaskinellResult processRegel(RegelMaskinellRequest regelResult);
 }
