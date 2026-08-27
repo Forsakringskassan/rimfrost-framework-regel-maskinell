135c1a6e5c7d811b3e78cae5ef5dd89dfa061d13
Nils Elveros <nils.elveros@tromb.com>
2026-08-27 15:14:10 +0200

    fix: minor javadoc update

--- src/test/java/se/fk/rimfrost/framework/regel/maskinell/helpers/WireMockRegelMaskinell.java
+++ src/test/java/se/fk/rimfrost/framework/regel/maskinell/helpers/WireMockRegelMaskinell.java
@@ -8,10 +8,8 @@ import java.util.Map;
 public class WireMockRegelMaskinell extends WireMockHandlaggning
 {
    /**
-    * Defines wiremock mappings for machine rules.
-    * <p>
-    * Note: Introducing this class creates a placeholder for adding mappings to all machine rules
-    * (even if initially no such mappings have been identified).</p>
+    * Extends the base handläggning WireMock mapping with machine-rule-specific
+    * properties, currently {@code referensdata.api.base-url}.
     *
     * @param server active WireMock server
     * @return property mappings
